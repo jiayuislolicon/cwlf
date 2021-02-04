0185bcbdffca5824b6d5b67aed1481131b23041d
lolicon <lovesyndra@gmail.com>
2021-02-04 18:19:44 +0800

    fix: add slidecontrol

--- src/components/molecules/SlideControl/index.js
+++ src/components/molecules/SlideControl/index.js
@@ -1,9 +1,19 @@
 import React from "react";
-// import { link } from "react-router-dom";
+import { ReactComponent as Arrow } from "static/svg/arrow.svg";
 import "./index.scss";
 
 const SlideControl = () => {
-  return <div className="slide-control">SlideControl</div>;
+  return (
+    <div className="slide-control">
+      <Arrow className="arrow arrow-left" />
+      <div className="slide-num">
+        <span className="now-num">1</span>
+        <div className="slash" />
+        <span className="total-num en">10</span>
+      </div>
+      <Arrow className="arrow arrow-right" />
+    </div>
+  );
 };
 
 export default SlideControl;
