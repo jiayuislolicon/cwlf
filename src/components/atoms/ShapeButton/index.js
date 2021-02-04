07a62d5c3204defad31e7aae03ab6030f4937608
lolicon <lovesyndra@gmail.com>
2021-02-04 12:44:22 +0800

    feat: add shape button

--- src/components/atoms/ShapeButton/index.js
+++ src/components/atoms/ShapeButton/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-// import { link } from "react-router-dom";
+import { ReactComponent as Heart } from "static/svg/btn-heart.svg";
+import { ReactComponent as ShareIcon } from "static/svg/icon-share.svg";
 import "./index.scss";
 
 const ShapeButton = () => {
-  return <div className="shape-button">ShapeButton</div>;
+  return (
+    <div className="shape-button">
+      <Heart className="heart" />
+      <ShareIcon className="share" />
+    </div>
+  );
 };
 
 export default ShapeButton;
