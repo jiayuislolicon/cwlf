a09e56f1189e88fc029a2c64f9442f927cc6fdb2
lolicon <lovesyndra@gmail.com>
2021-02-04 12:44:41 +0800

    feat: add tab btn

--- src/components/atoms/TabButton/index.js
+++ src/components/atoms/TabButton/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-// import { link } from "react-router-dom";
+import classNames from "classnames";
 import "./index.scss";
 
-const TabButton = () => {
-  return <div className="tab-button">TabButton</div>;
+const TabButton = ({ text, isNow }) => {
+  return (
+    <div className={classNames("tab-button", isNow ? "now" : "")}>
+      <h5>{text}</h5>
+    </div>
+  );
 };
 
 export default TabButton;
--- src/components/atoms/TabButton/test/TabButton.stories.js
+++ src/components/atoms/TabButton/test/TabButton.stories.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { boolean } from "@storybook/addon-knobs";
 
 import TabButton from "../index";
 
 const stories = storiesOf("atoms/TabButton", module);
 
-stories.add("__interactive", () => <TabButton />);
+stories.add("__interactive", () => (
+  <TabButton text="哈哈屁眼" isNow={boolean("isNow", false)} />
+));
