18167fa99d75d9bc5f3a565e95e7b50c0ec38f31
lolicon <lovesyndra@gmail.com>
2021-02-08 15:59:50 +0800

    fix: add inner page rwd style

--- src/components/organisms/ArticlePopup/index.js
+++ src/components/organisms/ArticlePopup/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import TimeTag from "components/atoms/TimeTag";
 import Cross from "components/atoms/Cross";
 import ShapeButton from "components/atoms/ShapeButton";
+import bg from "static/svg/innerPage-bg.svg";
 
 import "./index.scss";
 
@@ -11,18 +12,23 @@ const ArticlePopup = () => {
     <div className="article-popup">
       <Cross />
       <div className="content-wrapper">
-        <img src="" alt="" />
+        <img
+          src="https://picsum.photos/625/418"
+          className="article-cover"
+          alt="封面"
+        />
         <h4>第十屆兒童節親子活動「兒童月Online」－台中</h4>
         <p>
           將品牌識別標誌在不同媒介運用時，會使用到之色彩或配色，提供統一的標準色號，便於未來應用在其他延伸項目；如CMYK色號用於印刷使用、RGB色號用於電腦螢幕觀看使用、PANTONE色號用於特別專色使用、HEX色號用於網站設計使用。
         </p>
-        <img src="" alt="" />
+        <img src="https://picsum.photos/625/418" alt="測試" />
       </div>
-      <div className="list-btn">
-        <TimeTag />
+      <div className="list-btns">
+        <TimeTag month="1" day="2" year="2020" />
         <div className="event-tag">兒童節</div>
         <ShapeButton />
       </div>
+      <img src={bg} className="article-bg ab-center" alt="背景" />
     </div>
   );
 };
