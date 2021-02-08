3e8fc2292630c54de989e45ca3c092eeeade80b7
lolicon <lovesyndra@gmail.com>
2021-02-09 01:33:58 +0800

    fix: limit slide max number

--- src/components/organisms/EventsWrapper/index.js
+++ src/components/organisms/EventsWrapper/index.js
@@ -15,7 +15,9 @@ import { useSelector } from "react-redux";
 const EventsWrapper = ({ mask }) => {
   const { width } = useSelector((state) => state.global);
   const [slideNum, setSlideNum] = useState(0);
+  const [slideControlValue, setSlideControlValue] = useState(0);
   const [itemWidth, setItemWidth] = useState(0);
+  const [max, setMax] = useState(0);
 
   const setCarouselItemWidth = () => {
     if (width >= 1440) {
@@ -40,27 +42,42 @@ const EventsWrapper = ({ mask }) => {
   }
 
   const addSlide = () => {
-    if (slideNum === 3) {
-      setSlideNum(0);
+    if (slideNum === (max - 3)) {
+      setSlideNum(max - 3);
     } else {
       setSlideNum(slideNum + 1);
     }
   }
 
   useEffect(() => {
+    setMax(4);
+  }, [])
+
+  useEffect(() => {
     setCarouselItemWidth();
   }, [width])
+  
+  useEffect(() => {
+    if (width < 768) {
+      setSlideControlValue(slideNum + 1);
+    } else if (width >= 768) {
+      if (slideNum > (max - 3)) {
+        setSlideNum(max - 3)
+      }
+      setSlideControlValue(slideNum + 3);
+    }
+  }, [slideNum, width])
 
   return (
     <section className="section-events">
       <h2>展演活動</h2>
       <TabWrapper nowIndex={1} />
       <Carousel
-      itemWidth={itemWidth}
-      offset={width < 1024 ? 14 : 28}
-      onChange={slideChange}
-      value={slideNum}
-    >
+        itemWidth={itemWidth}
+        offset={width < 1024 ? 14 : 28}
+        onChange={slideChange}
+        value={slideNum}
+      >
         <EventItem
           title="第十屆兒童節親子活動「兒童月Online」－台中"
           imgSrc="https://picsum.photos/625/520"
@@ -84,7 +101,7 @@ const EventsWrapper = ({ mask }) => {
         />
       </Carousel>
       {!mask && <WhiteCircle />}
-      <SlideControl nowNum={slideNum + 1} totalNum={10} leftClick={minusSlide} rightClick={addSlide} />
+      <SlideControl nowNum={slideControlValue} totalNum={max} leftClick={minusSlide} rightClick={addSlide} />
     </section>
   );
 };
