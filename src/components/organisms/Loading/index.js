abab32378aa7b90774c704d14452bda40fbd4e06
lolicon <lovesyndra@gmail.com>
2021-02-17 20:13:08 +0800

    fix: loading function

--- src/components/organisms/Loading/index.js
+++ src/components/organisms/Loading/index.js
@@ -1,35 +1,61 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable no-param-reassign */
+import React, { useState, useEffect } from "react";
 import LoadingLayout from "components/molecules/LoadingLayout";
 import "./index.scss";
 import { assets } from "assets";
 
+class InitImagePreload {
+  constructor(props) {
+    this.manifest = props.manifest;
+    this.temImg = [];
+    this.imgArray = [];
+    this.func = props.func;
+    this.load();
+  }
+
+  load() {
+    if (this.manifest.length > 0) {
+      this.manifest.forEach((item) => {
+        const img = new Image();
+        img.src = item;
+        img.onload = () => {
+          this.imgArray.push(img);
+          const percent = Math.floor(
+            (this.imgArray.length / this.manifest.length) * 100
+          );
+          this.func(percent);
+        };
+
+        this.temImg.push(img);
+      });
+    } else {
+      this.func(100);
+    }
+  }
+
+  destroy() {
+    this.temImg.forEach((item) => {
+      item.onload = () => {};
+    });
+  }
+}
+
 const Loading = () => {
   const [loadingNumber, setLoadingNumber] = useState(0);
-  const cacheImages = async (srcArr) => {
-    const promises = await srcArr.map((src) => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-
-        img.src = src;
-        img.onload = resolve("s");
-        img.onerror = reject();
-      });
-    });
-
-    await Promise.all(promises).then(() => {
-      setLoadingNumber(3);
-    });
-  };
 
   useEffect(() => {
-    cacheImages(assets);
+    const loading = new InitImagePreload({
+      manifest: assets,
+      func: setLoadingNumber,
+    });
+    loading.load();
+
+    return () => loading.destroy();
   }, []);
 
   return (
     <div className="section-loading">
-      <LoadingLayout
-        number={Math.floor((loadingNumber / assets.length) * 100)}
-      />
+      <LoadingLayout number={loadingNumber / 2} />
     </div>
   );
 };
